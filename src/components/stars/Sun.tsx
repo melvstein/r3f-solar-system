2f2bd626d7f8f9772e3993de98169134ab392a81
melvstein <melvinbayogo@gmail.com>
2025-04-23 22:59:18 +0800

    solar system

--- src/components/stars/Sun.tsx
+++ src/components/stars/Sun.tsx
@@ -1,6 +1,6 @@
-import { useTexture } from "@react-three/drei";
+import { Html, useTexture } from "@react-three/drei";
 import { TStar } from "../../utils/types";
-import { forwardRef, RefObject } from "react";
+import { forwardRef, RefObject, useState } from "react";
 import { Mesh } from "three";
 import { useFrame } from "@react-three/fiber";
 import { adjustSpeed } from "../../utils/configs";
@@ -8,8 +8,9 @@ import { adjustSpeed } from "../../utils/configs";
 const Sun = forwardRef<Mesh, {}>((_, ref) => {
     const meshRef = ref as RefObject<Mesh>;
     const sunTexture = useTexture('/assets/textures/2k_sun.jpg');
+    const [hover, setHover] = useState<boolean>(false);
 
-    const sun: TStar = {
+    const star: TStar = {
         name: "Sun",
         radius: 6,
         rotationSpeed: 0.004,
@@ -17,14 +18,35 @@ const Sun = forwardRef<Mesh, {}>((_, ref) => {
     };
 
     useFrame(() => {
-        meshRef.current?.rotateY(sun.rotationSpeed * adjustSpeed);
+        meshRef.current?.rotateY(star.rotationSpeed * adjustSpeed);
     });
 
+console.log(hover);
     return (
-        <mesh ref={ref}>
-            <sphereGeometry args={[sun.radius, 35, 35]} />
-            <meshBasicMaterial map={sun.texture} />
-        </mesh>
+        <group ref={ref}>
+            <mesh onPointerOver={() => { setHover(true) }} onPointerOut={ () => { setHover(false) } }>
+                <sphereGeometry args={[star.radius, 35, 35]} />
+                <meshBasicMaterial map={star.texture} />
+            </mesh>
+            { hover && 
+                <Html
+                    position={[0, star.radius + 0.5, 0]} // float label above the planet
+                    style={{
+                    background: 'rgba(0,0,0,0.6)',
+                    color: 'white',
+                    padding: '4px 8px',
+                    borderRadius: '4px',
+                    fontSize: '150px',
+                    }}
+                    className="bg-black/5 text-white p-4 round-lg text-lg"
+                    center
+                    distanceFactor={10}
+                    occlude
+                >
+                    { star.name }
+                </Html>
+            }
+        </group>
     );
 });
 
